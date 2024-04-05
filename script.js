2d1abf361dee96cfdc286f1901e253721df2e367
honey <47128674+SyrymAbdikhan@users.noreply.github.com>
2024-04-05 23:34:06 +0500

    added "copied" sign

--- script.js
+++ script.js
@@ -2,10 +2,11 @@
 const input = document.getElementById("input");
 const table = document.getElementById("table");
 const raw = document.getElementById("raw");
+const btn = document.getElementById("btn");
 
 
 input.addEventListener('input', function () {
-    let new_table = "<table>";
+    let new_table = "<table><tbody>";
 
     let lines = this.value.split(/[\r\n]+/);
     for (let i = 0; i < lines.length; i++) {
@@ -14,7 +15,7 @@ input.addEventListener('input', function () {
         new_table += `<tr><td>${words[0]}</td><td>${text}</td></tr>`;
     }
 
-    new_table += "</table>";
+    new_table += "</tbody></table>";
     table.innerHTML = new_table;
     raw.value = new_table;
 });
@@ -23,4 +24,8 @@ function copy() {
     raw.select();
     raw.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(raw.value);
+    btn.innerText = 'copied';
+    setTimeout(function () {
+        btn.innerText = 'copy raw';
+    }, 1500);
 }
