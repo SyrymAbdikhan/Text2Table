587088b814e8a94a3afc13c4d59ad440bb6c6740
honey <47128674+SyrymAbdikhan@users.noreply.github.com>
2024-04-06 16:09:17 +0500

    minor enhancements

--- script.js
+++ script.js
@@ -5,17 +5,19 @@ const raw = document.getElementById("raw");
 const btn = document.getElementById("btn");
 
 
-input.addEventListener('input', function () {
-    let new_table = "<table><tbody>";
-
-    let lines = this.value.split(/[\r\n]+/);
+input.addEventListener("input", function () {
+    let new_table = "";
+    let lines = this.value.trim().split(/[\r\n]+/);
     for (let i = 0; i < lines.length; i++) {
-        let words = lines[i].split(" ");
+        if (lines[i].length === 0) continue;
+        let words = lines[i].split(/\s+/);
         let text = words.slice(1, words.length).join(" ");
         new_table += `<tr><td>${words[0]}</td><td>${text}</td></tr>`;
     }
 
-    new_table += "</tbody></table>";
+    if (new_table)
+        new_table = "<table><tbody>" + new_table + "</tbody></table>";
+
     table.innerHTML = new_table;
     raw.value = new_table;
 });
@@ -24,8 +26,8 @@ function copy() {
     raw.select();
     raw.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(raw.value);
-    btn.innerText = 'copied';
+    btn.innerText = "copied";
     setTimeout(function () {
-        btn.innerText = 'copy raw';
+        btn.innerText = "copy raw";
     }, 1500);
 }
